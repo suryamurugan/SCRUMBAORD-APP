d6c20f3c672a93724543fb7dada51d5bb277f44b
suryamurugan <surya3542live@gmail.com>
2018-04-11 18:20:29 +0530

    Done till chap 7 Just auth left

--- djangular/djangular/urls.py
+++ djangular/djangular/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from  django.conf.urls import url,include
+from  django.views.generic import  TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^scrumboard/',include('scrumboard.urls')),
+    url(r'^$', TemplateView.as_view(template_name="scrumboard/home.html")),
 ]
