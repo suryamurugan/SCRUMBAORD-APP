12ccbf9b358b9b759656f387fea1a77839b89ea8
suryamurugan <surya3542live@gmail.com>
2018-04-11 15:33:10 +0530

    Done with basic models and stuff ! next up djRest

--- djangular/djangular/urls.py
+++ djangular/djangular/urls.py
@@ -1,4 +1,4 @@
-"""scrumboard URL Configuration
+"""djangular URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
